41ebfe2692ce6b0ac56fdd19f5d901286ed2eb6c
caseyjakemorris <caseyjakemorris@gmail.com>
2024-11-26 22:18:01 -0500

    new commit

--- 0-authentication.tf
+++ 0-authentication.tf
@@ -1,1 +1,14 @@
-provider 
+# provider
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.78.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  # Configuration options
+} 
--- 1-VPC.tf
+++ 1-VPC.tf
@@ -1,1 +1,5 @@
 # VPC file 
+# Create a VPC
+resource "aws_vpc" "AppA" {
+  cidr_block = "10.0.0.0/16"
+}
